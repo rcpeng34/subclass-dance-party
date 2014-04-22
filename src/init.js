4f5abb9b508c3cd037bfae5d4ff2873b497cbcd7
Hack Reactor Students <rcpeng@gmail.com>
2014-04-22 10:28:45 -0700

    put lineup button back in

--- src/init.js
+++ src/init.js
@@ -23,18 +23,22 @@ $(document).ready(function(){
     // make a dancer with a random position
 
     var dancer = new dancerFunction(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
       Math.random() * 1000,
       window.dancers.length
     );
+
     $('.dancefloor').append(dancer.$node);
     window.dancers.push(dancer);
+
   });
 
-  $(".LineUpButton").on("click", function(event){
+  $('.LineUpButton').on('click', function(event){
     for(var i = 0; i < window.dancers.length; i++) {
-      window.dancers[i].setPosition([$("body").height()*0.6, $(".dancefloor").width() - 100*i])
+      window.dancers[i].setPosition([
+        $('body').height()*0.6, $('.dancefloor').width() - 100*i
+      ]);
     }
   });
 
