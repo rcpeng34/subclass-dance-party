09a48651947d3f3aa62df6f4b16bfd15686af37c
Hack Reactor Students <rcpeng@gmail.com>
2014-04-21 23:53:27 -0700

    now requires a click to take money from dancers

--- src/dancerFunctions.js
+++ src/dancerFunctions.js
@@ -9,8 +9,6 @@ $(document).ready(function(){
     dancer = window.dancers[this.id];
     console.log(this.id, this)
     $('.popbox').text("money in wallet: $" + dancer.money);
-    dancer.money = Math.max(0, dancer.money-10);
-    // $('.popbox').css({position})
     $('.popbox').show();
   });
 
@@ -18,5 +16,17 @@ $(document).ready(function(){
   .on('mouseleave', '.dancer', function(){
     $('.popbox').hide();
   });
+
+  $('.dancefloor')
+  .on('click', '.dancer', function(){
+    dancer.money -= 10;
+  });
+
+  //findDistance takes arrays
+  // var findDistance = function (pos1, pos2) {
+  //   var y = pos1[0] - pos2[0];
+  //   var x = pos1[1] - pos2[1];
+  //   return Math.sqrt(y*y + x*x);
+  // };
 
 });
