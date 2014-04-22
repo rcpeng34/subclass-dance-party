42140b9f1e0df246b3cf07e4f3ebc4bd1fa48a4f
Hack Reactor Students <rcpeng@gmail.com>
2014-04-22 11:54:18 -0700

    ballers now start with  or more, they leave the club when they run out of money

--- src/BallerDancer.js
+++ src/BallerDancer.js
@@ -1,10 +1,13 @@
 var BallerDancer = function(top, left, timeBetweenSteps, dancerIndex) {
   Dancer.apply(this, Array.prototype.slice.call(arguments));
+  // ballers start with at least $400
+  this.money = Math.max((Math.floor(Math.random()*1000)), 400);
   this.$node.attr({
     src: 'http://cdn.freebievectors.com/illustrations/10/b/business-man-clip-art/preview.jpg',
     width: '50px',
     height: '100px'
   });
+  this.isAtTheClub = true;
 };
 
 BallerDancer.prototype = Object.create(Dancer.prototype);
@@ -13,7 +16,11 @@ BallerDancer.prototype.constructor = BallerDancer;
 BallerDancer.prototype.step = function () {
   // this.oldstep();
   Dancer.prototype.step.apply(this);
-  this._position[0] += (Math.random()*40)-20;
-  this._position[1] += (Math.random()*40)-20;
-  this.setPosition(this._position);
+  this.randomMovement();
+  if (this.money < 10 && this.isAtTheClub) {
+    this.isAtTheClub = false;
+    window.dancers.splice(this._id, 1);
+    this.leaveTheClub();
+
+  }
 };
